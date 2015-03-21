9c4d85398d646ec358e9bcf72bdc98761fbf798f
epocoursera <epo.coursera@gmail.com>
2015-03-21 19:26:50 +0100

    Assignment 02 completed including the following functions : makeCacheMatrix cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Only for myself - set working directory
+setwd("~/Github/ProgrammingAssignment2")
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+######## START ASSIGNMENT 02 ########
+makeCacheMatrix <- function(x=matrix()) {
+    
+    # This function creates a list of functions to 
+    # set the value of the matrix
+    # get the value of the matrix
+    # set the value of inverse of the matrix
+    # get the value of inverse of the matrix
+    
+    # Set an empty Matrix
+    m <- NULL 
+    
+    # set the value of the matrix
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+  
+    # Returns the value of the matrix
+    get <- function() x
+    
+    # set the value of inverse of the matrix
+    setInverse <- function(inverse) m <<-inverse
+    
+    # get the value of inverse of the matrix
+    getInverse <- function() m
+    
+    # Creates a list of the functions 
+    list(set = set, get = get,
+        setInverse = setInverse,
+        getInverse = getInverse)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Computes the inverse of the matrix returned thanks to makeCacheMatrix()
+    
+    m <- x$getInverse()
+    
+    # If the value of the inverse is not null, then returns the value
+    if ( ! is.null(m)) {
+        print("getting cached data")
+        return(m)
+    }
+  
+    # Set inverse of the value
+    m <- solve(x$get())
+    x$setInverse(m)
+    
+    # Returns the Inverse value
+    m 
 }
+######## END ASSIGNMENT 02 ########
